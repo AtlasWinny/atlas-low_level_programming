426a1f9445692f4db37c723fffb5b04201cce055
Winston Cannon <winstonjcannon@hotmail.com>
2024-01-23 01:22:32 +0000

    1-alphabet.c

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,16 +1,16 @@
-#ifndef main_h
-	#define main_h
+#include "main.h"
 
 /**
- * print_alphabet - Prints the alphabet in lowercase.
+ * print_alphabet - prints lower case alphabet
  *
- * Return: Always 0 (Success)
- */
+ **/
+
 void print_alphabet(void)
 {
-	int i;
 
-	for (i = 97; i <= 122; i++)
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
 	{
 		_putchar(i);
 	}
