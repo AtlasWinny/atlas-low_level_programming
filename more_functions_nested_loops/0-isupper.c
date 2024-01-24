fe0af1d09aceb5746f9271df784513b6976f55f9
Winston Cannon <winstonjcannon@hotmail.com>
2024-01-24 17:41:05 +0000

    main.cfix

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,18 +1,15 @@
 #include "main.h"
-
+#include <stdio.h>
 /**
- * _isupper - checks if parameter is an uppercase character.
- * 
- * Return: 0
- */
+*_isupper - function that verifies if a  character is uppercase or not
+*@c: tested character
+*Return: returns 1 if it is uppercase , 0 if not
+*/
+
 int _isupper(int c)
 {
-	if (c >= 65 && c <= 90)
-	{
-		return (1);
-	}
-	else
-	{
-		return (0);
-	}
+if ((c >= 'A') && (c <= 'Z'))
+return (1);
+
+return (0);
 }
