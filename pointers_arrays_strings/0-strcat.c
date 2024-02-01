3a1036889ef3827a519982bb4d5bd9bdd699b6eb
Winston Cannon <winstonjcannon@hotmail.com>
2024-02-01 17:52:34 +0000

    strfix3

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -1,31 +1,26 @@
 #include "main.h"
-#include <stdio.h>
-
 /**
- * main - checks code
- *
- * return - always dest
- *
+ * _strcat - concatenates two strings,
+ * 
+ * 
+ * 	return always (dest)
  */
+char *_strcat(char *dest, char *src)
+{
+	int count = 0, count2 = 0;
 
-char *_strcat(char *dest, char *src);
-
-{	
-	int i;
-	int j;
-
-	i = 0;
-	while (dest[i] != '\0')
-	{	
-	i++;
-	}	
-	j = 0;
-	while (src[j] != '\0')
+	while (*(dest + count) != '\0')
 	{
-	dest[i] = src[j];
-	i++;
-	j++;
+		count++;
 	}
-	dest[i] = '\0';
+
+	while (count2 >= 0)
+	{
+		*(dest + count) = *(src + count2);
+		if (*(src + count2) == '\0')
+			break;
+		count++;
+		count2++;
+	}
 	return (dest);
 }
